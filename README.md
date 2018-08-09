b5b6aa60f9fff0c94b10a70eecd816886c2661d4
Florian Schüller <florian.schueller@gmail.com>
2018-08-09 23:35:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Many Users
+Quick and dirty script for ubuntu to let other people work on your PC at the same time
---
+++ find_input_and_start.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+sudo apt install xserver-xephyr evtest
+
+sudo rm -rf /tmp/test-fifo
+mkfifo /tmp/test-fifo
+check() {
+	sudo evtest $1|grep "^Event: "|dd bs=200 count=1 >/dev/null 2>&1 && echo "$1" >> /tmp/test-fifo
+}
+
+declare -a pids
+echo "Starting listeners"
+for e in $( ls /dev/input/event* ); do
+	check $e >/dev/null 2>&1 &
+	p=$(ps --ppid $! -o pid=)
+	pids+=("$p")
+done
+echo "done"
+
+echo "Please move the mouse"
+export MOUSE=$(cat /tmp/test-fifo|grep -m 1 "/dev/input/.*")
+
+
+echo "Please press Ctrl often"
+export KEYBOARD=$(cat /tmp/test-fifo|grep -m 1 "/dev/input/.*")
+
+echo Thank you
+
+sleep 5
+
+echo "Stopping listeners"
+#sudo kill ${pids[*]} >/dev/null 2>&1
+sudo killall evtest
+wait ${pids[*]} >/dev/null 2>&1
+echo "done"
+
+sudo rm -rf /tmp/test-fifo
+
+echo "The mouse is $MOUSE"
+echo "The keyboard is $KEYBOARD"
+
+echo "Let's go"
+./X11SimpleMultiseat.sh
